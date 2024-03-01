a9507f46e2e121fa347206883a33bd1bcd34f352
Giulioneri1 <giulio.neri@outlook.it>
2024-03-01 11:10:59 +0100

    add eslint configs

--- src/backend/index.js
+++ src/backend/index.js
@@ -67,7 +67,7 @@ app.post('/api/notes', (request, response, next) => {
     .catch(err => next(err))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
     .then(result => {
       response.status(204).end()
